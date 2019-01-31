ddc68d053c10ebfaa5d133adf63c7b6c480ac032
Marcin Szymanski <ms32035@gmail.com>
2019-01-31 17:19:35 +0000

    update some layouts

--- inspector/checks/forms.py
+++ inspector/checks/forms.py
@@ -1,6 +1,6 @@
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, Row, Column, Div
 from django import forms
 
 from .models import CheckGroup, Datacheck, CheckRun, EnvironmentStatus
@@ -25,11 +25,31 @@ class CheckGroupForm(forms.ModelForm):
 
 
 class DatacheckForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.layout = Layout(
+        Row(
+            Column('code', css_class='form-group col-md-8 mb-0'),
+            Column('group', css_class='form-group col-md-3 mb-0'),
+            Column('weight', css_class='form-group col-md-1 mb-0'),
+            css_class='form-row'
+        ),
+        Row(
+            Column('description', css_class='form-group col-md-12 mb-0'),
+            css_class='form-row'
+        ),
+        Row(Div(Div(css_class="card_header"),css_class="card")
+
+        ),
+        Submit('submit', 'Submit', css_class="btn-sm"))
+
     class Meta:
         model = Datacheck
         fields = ['code', 'description', 'weight', 'left_system', 'left_type', 'left_logic', 'relation', 'right_system',
                   'right_type', 'right_logic',
                   'supports_warning', 'warning_relation', 'warning_type', 'warning_logic', 'group']
+        widgets = {
+            'description': forms.Textarea({'cols': 40, 'rows': 3})
+        }
 
 
 class CheckRunForm(forms.ModelForm):
--- inspector/systems/forms.py
+++ inspector/systems/forms.py
@@ -1,9 +1,20 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Row, Column
 from django import forms
 
 from .models import Environment, Instance, System
 
 
 class SystemForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.layout = Layout(
+        Row(
+            Column('name', css_class='form-group col-md-6 mb-0'),
+            Column('application', css_class='form-group col-md-6 mb-0'),
+            css_class='form-row'
+        ),
+        Submit('submit', 'Submit', css_class="btn-sm"))
+
     class Meta:
         model = System
         fields = ['name', 'application']
@@ -22,3 +33,23 @@ class InstanceForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput()
         }
+
+    helper = FormHelper()
+    helper.layout = Layout(
+        Row(
+            Column('system', css_class='form-group col-md-6 mb-0'),
+            Column('environment', css_class='form-group col-md-6 mb-0'),
+            css_class='form-row'
+        ),
+        Row(
+            Column('host', css_class='form-group col-md-8 mb-0'),
+            Column('port', css_class='form-group col-md-1 mb-0'),
+            Column('database_or_schema', css_class='form-group col-md-3 mb-0'),
+            css_class='form-row'
+        ),
+        Row(
+            Column('login', css_class='form-group col-md-6 mb-0'),
+            Column('password', css_class='form-group col-md-6 mb-0'),
+            css_class='form-row'
+        ),
+        Submit('submit', 'Submit', css_class="btn-sm"))
