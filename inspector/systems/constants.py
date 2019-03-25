60803c80f3a5c8ecda3f6cb65843bda55fb01163
Marcin Szymański <ms32035@gmail.com>
2019-03-25 20:36:19 +0000

    add mysql support (#7)

--- inspector/systems/constants.py
+++ inspector/systems/constants.py
@@ -2,5 +2,6 @@ from model_utils import Choices
 
 APPLICATIONS = Choices(
     (0, 'POSTGRES', 'Postgresql'),
-    (1, 'REDSHIFT', 'Redshift')
+    (1, 'REDSHIFT', 'Redshift'),
+    (2, 'MYSQL', 'MySQL')
 )
