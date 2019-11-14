1f6e95a19bd844f98d471948809a89058b9ce542
Marcin Szymanski <ms32035@gmail.com>
2019-11-14 21:33:25 +0100

    modify imports

--- inspector/checks/admin.py
+++ inspector/checks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.formats.base_formats import JSON
 
 from .models import Datacheck, CheckGroup
 
@@ -12,6 +13,8 @@ class CheckGroupAdmin(admin.ModelAdmin):
 class DatacheckResource(resources.ModelResource):
     class Meta:
         model = Datacheck
+        import_id_fields = ["code"]
+        exclude = ["id"]
 
 
 class DatacheckAdmin(ImportExportModelAdmin):
@@ -23,6 +26,7 @@ class DatacheckAdmin(ImportExportModelAdmin):
         ("Warning", {"fields": ("supports_warning", "warning_type", "warning_logic")}),
     )
     resource_class = DatacheckResource
+    formats = (JSON,)
 
 
 admin.site.register(Datacheck, DatacheckAdmin)
